9d6f779a09f1f4fec0cfc5e7416a70881ea4ab71
Paolo Agostinetto <paul.ago@gmail.com>
2011-05-03 16:21:34 +0200

    Added at-least-one-checked: checks if at least one checkbox is checked :)

--- form-helper.js
+++ form-helper.js
@@ -8,9 +8,10 @@ $.fn.validateForm = function(opts){
 		onError: function(errString, field){ alert("Form Error: "+errString+" value: "+field); },
 		onSubmit: function(form){},
 		errorMsgs: {
-			filled: "The field must be filled",
-			integer: "You must prompt a valid integer",
-			email: "You must prompt a valid email"
+			'filled': "The field must be filled",
+			'integer': "You must prompt a valid integer",
+			'email': "You must prompt a valid email",
+			'at-least-one-checked': "You should check at least one option"
 		},
 		eachField: function(obj){}
 	};
@@ -27,13 +28,26 @@ $.fn.validateForm = function(opts){
 		'email': 	/^[\w_\-\.]+@[\w_\-\.]+\.[\w]{2,3}$/,
 	}
 
+	var alternateValidations = {
+		'at-least-one-checked': function(field){
+			var name = field.attr("name");
+			if($('input[name='+name+']:checked').length<1){
+				return false;
+			}
+			return true;
+		}
+	}
+
 	return this.each(function(){
 		var $this = $(this);
+		var app = {};
 
 		// Fire the callback for each field
 		$this.find("input, select, textarea").each(function(){
 			var field = $(this);
-			for(v in validations){
+
+			$.extend(app, validations, alternateValidations)
+			for(v in app){
 				if(field.is(".validate-"+v)){
 					settings.eachField(field);
 				}
@@ -41,16 +55,28 @@ $.fn.validateForm = function(opts){
 		});
 
 		$this.bind("submit", function(e){
-			settings.onSubmit($this);
+
+			settings.onSubmit();
+			
 			/// TODO :: check if the fields are disabled ??
 			$this.find("input, select, textarea").each(function(){
 				var field = $(this);
-				for(v in validations){
+
+				for(v in app){
 					if(field.is(".validate-"+v)){
-						if(!validations[v].test(field.val())){
-							// Error!!
-							settings.onError(settings.errorMsgs[v], field);
-							e.preventDefault();
+						if(typeof validations[v] == "object"){
+							if(!validations[v].test(field.val())){
+								// Error!!
+								settings.onError(settings.errorMsgs[v], field);
+								e.preventDefault();
+							}
+						}
+						else if(alternateValidations[v]){
+							if(!alternateValidations[v](field)){
+								// Error!!
+								settings.onError(settings.errorMsgs[v], field);
+								e.preventDefault();
+							}
 						}
 					}
 				}
